c1dba23964451cf18a7eefc55c171e7b12fe2370
Kaio Felipe <kaio.felipe0671@gmail.com>
2024-10-03 20:58:00 -0300

    feat: get product by id

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -25,4 +25,15 @@ public class ProdutosController : ControllerBase
 
         return produtos;
     }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos
+            .FirstOrDefault(x => x.ProdutoId == id);
+
+        if (produto is null) return NotFound();
+
+        return produto;
+    }
 }
