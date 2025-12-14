5741f018e79221f82b6f1aaf7cfee5cf2a124fbf
Kaio Felipe <kaio.felipe0671@gmail.com>
2025-12-14 18:36:23 -0300

    feat: api filter

--- APICatalogo/APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/APICatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Filters;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +28,7 @@ namespace APICatalogo.Controllers
         }
 
         [HttpGet]
+        [ServiceFilter(typeof(ApiLoggingFilter))]
         public async Task<ActionResult<IEnumerable<Categoria>>> GetAsync()
         {
             try
